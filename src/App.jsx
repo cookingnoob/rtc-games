5db09ad41befb9fe444af81c33bf14a190326cbc
alan <alan@iMac-de-alan.local>
2023-11-14 08:42:30 -0600

    first commit, added basic layout for tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Tic Tac Toe</h1>
+    <button>Inicia la partida</button> <br />
+    <button>X</button>
+    <button>O</button>
+    <h2>Es turno de ____</h2>
+    <br />
+    <button></button><button></button><button></button>
+    <br />
+    <button></button><button></button><button></button>
+    <br />
+    <button></button><button></button><button></button>
+    </>
+  )
+}
+
+export default App
