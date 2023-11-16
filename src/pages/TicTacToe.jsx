43f569c42df3d3b3ba5744018fb2921aa71cb609
cookingnoob <costealan@gmail.com>
2023-11-15 18:00:56 -0600

    added state for the player value and choosePlayerValue to send to the cell the selected checker

--- src/pages/TicTacToe.jsx
+++ src/pages/TicTacToe.jsx
@@ -2,27 +2,31 @@ import React, { useState } from 'react'
 
 const TicTacToe = () => {
   const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null])
+  const [player, setPlayer] = useState(null);
 
-  const handleClick = (index) => {
+  const handleBoardClick = (index) => {
     if (board[index] != null) {
       return
     }
     const newBoard = [...board];
-    newBoard[index] = 'X';
+    newBoard[index] = player;
     setBoard(newBoard)
   }
 
+  const choosePlayerValue = (e) => {
+    setPlayer(e.target.value)
+  }
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <button>Inicia la partida</button> <br />
-      <button>X</button>
-      <button>O</button>
+      <button onClick={choosePlayerValue} value={'X'}>X</button>
+      <button onClick={choosePlayerValue} value={'O'}>O</button>
       <h2>Es turno de ____</h2>
       <br />
       <div className='board'>
         {board.map((cell, index) => {
-          return <button key={index} onClick={() => handleClick(index)} value={cell}>{cell}</button>
+          return <button key={index} onClick={() => handleBoardClick(index)} value={cell}>{cell}</button>
         })}
       </div>
     </>
