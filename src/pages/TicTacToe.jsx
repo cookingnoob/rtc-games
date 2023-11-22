a4c2818eaadd0207701b84a81d198fe79051a45e
alan <alan@iMac-de-alan.local>
2023-11-21 20:15:52 -0600

    added text state to inform the player the status of the game, updated restart button to restart all states, added computerValue function

--- src/pages/TicTacToe.jsx
+++ src/pages/TicTacToe.jsx
@@ -3,30 +3,47 @@ import React, { useState } from 'react'
 const TicTacToe = () => {
   const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null])
   const [player, setPlayer] = useState(null);
+  const [computer, setComputer] = useState(null)
+  const [text, setText] = useState('Escoge una ficha');
+
   const handleBoardClick = (index) => {
     if (board[index] != null) {
       return
+    } else if (player == null){
+      setText('no has escogido una ficha')
     }
     const newBoard = [...board];
     newBoard[index] = player;
     setBoard(newBoard)
   }
 
+  const computerValue = () => {
+    if (player === 'X'){
+      setComputer('O');
+      console.log(computer)
+    }else if (player === 'O'){
+      setComputer('X');
+      console.log(computer)
+    }
+  }
+
   const choosePlayerValue = (e) => {
     setPlayer(e.target.value)
-
+    computerValue()
   }
 
   const resetGame = () => {
     setBoard([null, null, null, null, null, null, null, null, null])
+    setPlayer(null)
+    setText('Escoge una ficha')
   }
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Inicia la partida</button> <br />
-      <button onClick={choosePlayerValue} value={'X'} className=''>X</button>
-      <button onClick={choosePlayerValue} value={'O'} className=''>O</button>
-      <h2>Es turno de ____</h2>
+      <button onClick={choosePlayerValue} value={'X'} className='checkerValue'>X</button>
+      <button onClick={choosePlayerValue} value={'O'} className='checkerValue'>O</button>
+      <h2>{text}</h2>
       <br />
       <div className='board'>
         {board.map((cell, index) => {
