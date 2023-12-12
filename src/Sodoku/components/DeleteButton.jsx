24cf12316e9383d13815cc91d24079ed93788d1d
alan <alan@iMac-de-alan.local>
2023-12-12 16:30:32 -0600

    added if statement to avoid deleting original values on DeleteButton

--- src/Sodoku/components/DeleteButton.jsx
+++ src/Sodoku/components/DeleteButton.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const DeleteButton = ({grid, cellIndex, sudokuBoard, setSudokuBoard}) => {
+const DeleteButton = ({grid, cellIndex, sudokuBoard, setSudokuBoard,copyOfSudoku, setText }) => {
     const handleDelete = () => {
-            const arrayToBeChanged = [...sudokuBoard]
-            arrayToBeChanged[grid][cellIndex] = null
-            setSudokuBoard(arrayToBeChanged)
+      if(copyOfSudoku[grid][cellIndex] !== null){
+        setText('No se pueden cambiar los números originales')
+        return
+    }else{
+      const arrayToBeChanged = [...sudokuBoard]
+      arrayToBeChanged[grid][cellIndex] = null
+      setSudokuBoard(arrayToBeChanged)
+    }      
     }
   return (
     <button onClick={handleDelete}>Borrar</button>
--- src/Sodoku/components/PlayingSudokuWindow.jsx
+++ src/Sodoku/components/PlayingSudokuWindow.jsx
@@ -16,7 +16,7 @@ const PlayingSudokuWindow = ({sudokuBoard, copyOfSudoku, setSudokuBoard, makepuz
     <Board sudokuBoard={sudokuBoard} setGrid={setGrid} setCellIndex={setCellIndex} setText={setText}/>
     <NumbersButtons grid={grid} cellIndex={cellIndex} sudokuBoard={sudokuBoard} setText={setText} setNumberToAdd={setNumberToAdd}/>
     <AddValidNumber copyOfSudoku={copyOfSudoku} numberToAdd={numberToAdd} setText={setText} cellIndex={cellIndex} grid={grid} setSudokuBoard={setSudokuBoard} sudokuBoard={sudokuBoard} setNumberToAdd={setNumberToAdd} setGrid={setGrid} setCellIndex={setCellIndex}/>
-    <DeleteButton grid={grid} cellIndex={cellIndex} sudokuBoard={sudokuBoard} setSudokuBoard={setSudokuBoard} />
+    <DeleteButton grid={grid} copyOfSudoku={copyOfSudoku} setText={setText} cellIndex={cellIndex} sudokuBoard={sudokuBoard} setSudokuBoard={setSudokuBoard} />
     <StartSudoku  setSudokuBoard={setSudokuBoard} setCopyOfSudoku={setCopyOfSudoku} makepuzzle={makepuzzle} buttonName={'Nuevo tablero'}/>
     <RestartSudoku copyOfSudoku={copyOfSudoku} setSudokuBoard={setSudokuBoard}/>
     </>
