fc7932edb56f798eff42cf6d07b105611f88a6ee
alan <alan@iMac-de-alan.local>
2023-12-21 11:13:52 -0600

    fixed clues on Hangman that lingered on the next game, also it shows the word when winning

--- src/Hangman/components/WinWindow.jsx
+++ src/Hangman/components/WinWindow.jsx
@@ -1,10 +1,13 @@
-
+import { useContext } from 'react'
 import StartButton from './StartButton'
+import { HangmanContext } from '../HangmanContext'
 
 const WinWindow = () => {
+  const {wordToGuess} = useContext(HangmanContext)
   return (
     <>    
-    <h1>Ganaste!! 🥳</h1> 
+    <h1>Ganaste!! 🥳,</h1> 
+    <h1>{wordToGuess}</h1>
     <StartButton/></>
   )
 }
--- src/Hangman/hooks/useStartHangman.jsx
+++ src/Hangman/hooks/useStartHangman.jsx
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import { HangmanContext } from "../HangmanContext";
 
 const useStartHangman = () => {
-    const {setWordToGuess, listOfWords, setStrike, setLetterButtons} = useContext(HangmanContext)
+    const {setWordToGuess, listOfWords, setStrike, setLetterButtons, setClue} = useContext(HangmanContext)
     const startGame = () => {
           const randomNumber = Math.floor(Math.random() * listOfWords.length);
           const selectedWord = listOfWords[randomNumber];
           setWordToGuess(selectedWord);
           setStrike('');
           setLetterButtons(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
-          
+          setClue('')
       }
   return {startGame}
 }
